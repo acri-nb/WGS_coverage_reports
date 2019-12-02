ffb53886eb6fe759b04e00f93caf8f592b4c84a0
SupriseCestEric <33495590+SupriseCestEric@users.noreply.github.com>
2019-12-02 16:04:30 -0400

    Update Coverage_Analysis_GATK_WES.sh
    Some code was commented out

--- Coverage_Analysis_GATK_WES.sh
+++ Coverage_Analysis_GATK_WES.sh
@@ -5,6 +5,6 @@ OUTFILE=$4
 UCSC=$5
 BED=$6
 GL=$7
-#java -jar $JAR -T DepthOfCoverage -R $GENOME -o $OUTFILE -I $INFILE -L $BED -geneList $UCSC -ct 10 ;
+java -jar $JAR -T DepthOfCoverage -R $GENOME -o $OUTFILE -I $INFILE -L $BED -geneList $UCSC -ct 10 ;
 echo "Generating report for $(pwd)/$OUTFILE.sample_gene_summary";
 Rscript --vanilla -e "rmarkdown::render('HTML_CovReport_v1_WES.Rmd', knit_root_dir = '$(pwd)')" --args $OUTFILE.sample_gene_summary $GL $BED $UCSC $OUTFILE.sample_interval_summary $OUTFILE
